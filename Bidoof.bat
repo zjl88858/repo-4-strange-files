cee3ecef37f64aa275ac10c264c2931178fd2a50
神龟 <1033008052@qq.com>
2018-11-11 04:57:52 +0800

    Add files via upload

---
+++ Bidoof.bat
@@ -0,0 +1,12 @@
+@echo off
+title GuituAcc Runtime Broker Shell
+echo Don't Close This Shell Before You Disconnect Accel. You Can Minimize It.
+echo 不要关闭这个脚本在你断开加速之前。你可以最小化它。
+echo не закрывайте эту оболочку, прежде, чем вы отключите ускорение.ты можешь свести к минимуму.
+echo/
+echo/
+echo/
+echo Server:Bidoof 你正通过服务器:Bidoof连接互联网
+setproxy.exe on 127.0.0.1 1080
+daze.exe client -s "104.168.147.129:8080" -l "0.0.0.0:1080" -k "daze@3591342552908659" -e "asheshadow" -f "auto" -dns "1.2.4.8:53"
+exit

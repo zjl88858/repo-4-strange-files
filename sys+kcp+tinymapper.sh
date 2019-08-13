c332e3b39f58806dbc9d5b79e0fe6aef1369194f
神龟 <1033008052@qq.com>
2019-08-13 21:25:36 +0800

    Update sys+kcp+tinymapper.sh

--- sys+kcp+tinymapper.sh
+++ sys+kcp+tinymapper.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+echo "SystemConf+KCPTun+TinyMapper OneKey Installer"
+echo -e "\033[33m --- BY:Turmi 20190813 --- \033[0m"
+echo -e "\033[36minput 1.\033[0m调教系统配置"
+echo -e "\033[36minput 2.\033[0m安装KCPTun+TinyMapper"
+echo 

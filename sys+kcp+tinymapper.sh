0da05dbf8c3b561b4271f8895e7cd33aa4e88179
神龟 <1033008052@qq.com>
2019-08-13 22:12:50 +0800

    Update sys+kcp+tinymapper.sh

--- sys+kcp+tinymapper.sh
+++ sys+kcp+tinymapper.sh
@@ -1,19 +1,54 @@
 #!/bin/bash
 if cat /etc/issue | grep -q -E -i "debian"; then
-apt install whiptail -y
+apt-get install whiptail -y
 elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-apt install whiptail -y
+apt-get install whiptail -y
 else
 yum install whiptail -y
-OPTION=$(whiptail --title "兔子最可爱" --menu "Choose your option" 15 60 4 \
-"1" "Grilled Spicy Sausage" \
-"2" "Grilled Halloumi Cheese" \
-"3" "Charcoaled Chicken Wings" \
-"4" "Fried Aubergine"  3>&1 1>&2 2>&3)
- 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Your chosen option:" $OPTION
+fi
+OPTION=$(whiptail --title "SysConf+KCP+TM OneKey Dialog BY:TURMI" --menu "How many Tuzi u need? Ctrl+C to exit." 15 60 4 \
+"1xTuzi" "Modify sysctl.conf" \
+"2xTuzi" "Install KCPTun and add to rc.local" \
+"3xTuzi" "Install TinyMapper and add to rc.local" \
+"4xTuzi" "Uninstall KCPTun and TinyMapper" 3>&1 1>&2 2>&3)
+if [ $OPTION = 1xTuzi ]; then
+sed -i '/fs.file-max/d' /etc/sysctl.conf
+sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+sed -i '/net.ipv4.route.gc_timeout/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_synack_retries/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_syn_retries/d' /etc/sysctl.conf
+sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_max_orphans/d' /etc/sysctl.conf
+sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+echo "fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.core.somaxconn = 32768
+net.core.netdev_max_backlog = 32768
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_max_orphans = 32768
+net.ipv4.ip_forward = 1">>/etc/sysctl.conf
+sysctl -p
+echo "*               soft    nofile           1000000
+*               hard    nofile          1000000">/etc/security/limits.conf
+echo "ulimit -SHn 1000000">>/etc/profile
+whiptail --title "SysConf+KCP+TM OneKey Dialog BY:TURMI" --msgbox "Modify complete! Maybe need reboot system." 10 60
 else
     echo "You chose Cancel."
 fi
